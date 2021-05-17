7cecf5bf4e7b96c5c9ffd35cff1b266d974d72da
Dung Nguyen <contact@dungnt.net>
2021-05-17 11:48:43 +0700

    change method name

--- apps/__init__.py
+++ apps/__init__.py
@@ -11,7 +11,7 @@ from apps.v1 import starter
 
 def create_app():
     """An application factory, as explained here:
-    http://flask.pocoo.org/docs/patterns/appfactories/.
+    https://flask.pocoo.org/docs/patterns/appfactories/.
     """
     app = Flask(__name__)
 
@@ -20,8 +20,8 @@ def create_app():
 
     register_extensions(app)
     register_blueprints(app)
-    register_errorhandlers(app)
-    register_shellcontext(app)
+    register_error_handlers(app)
+    register_shell_context(app)
     register_commands(app)
 
     return app
@@ -47,7 +47,7 @@ def register_blueprints(app):
     app.register_blueprint(starter.routes.blueprint, url_prefix='/v1/starter')
 
 
-def register_errorhandlers(app):
+def register_error_handlers(app):
     def handle_json_error(error):
         response = jsonify(error.to_dict())
         # response = error.to_json()
@@ -57,7 +57,7 @@ def register_errorhandlers(app):
     app.errorhandler(JsonError)(handle_json_error)
 
 
-def register_shellcontext(app):
+def register_shell_context(app):
     """Register shell context objects."""
     pass
 
