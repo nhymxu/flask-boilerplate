13df12ea4eb874bfb7f89c9a5198a30680b72ef3
nhymxu <contact@dungnt.net>
2024-05-07 10:58:08 +0700

    feat: add favicon route

--- web.py
+++ web.py
@@ -1,8 +1,10 @@
+import base64
+import io
 import os
 
 from asgiref.wsgi import WsgiToAsgi
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, send_file
 
 from logics.auth_middleware import require_authentication
 from logics.error_handler import ErrorHandler
@@ -14,6 +16,17 @@ app.config['SERVER_AUTH_KEY'] = os.getenv('SERVER_AUTH_KEY')
 ErrorHandler(app)
 
 asgi_app = WsgiToAsgi(app)
+
+
+@app.get('/favicon.ico')
+def get_favicon():
+    # 1x1 transparent gif
+    b64_img = "R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+    b = base64.b64decode(b64_img.encode('utf-8'))
+    buf = io.BytesIO(b)
+    buf.seek(0)
+
+    return send_file(buf, mimetype="image/gif")
 
 
 @app.get('/')
